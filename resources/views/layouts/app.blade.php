bd6f05e348c7d78e431edf0d3a1c5aa81545e020
sammymutahi12@gmail.com <sammymutahi12@gmail.com>
2020-09-19 12:01:48 +0300

    show validation messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,17 @@
         </nav>
 
         <main class="py-4">
+            <div class="container">
+                @if($errors->any())
+                    <div class="alert alert-danger" role="alert">
+                        <ul class="mb-0">
+                            @foreach($errors->all() as $error)
+                                <li>{!! $error !!}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+            </div>
             @yield('content')
         </main>
     </div>
